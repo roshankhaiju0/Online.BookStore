8402151935a8825042cd51d508bbe1d74422a986
roshankhaiju0 <66667489+roshankhaiju0@users.noreply.github.com>
2020-06-09 13:44:10 +0545

    Add files via upload

---
+++ Online.Book-Store/main.js
@@ -0,0 +1,9 @@
+
+    document.getElementById('myCart').addEventListener('click', function(){
+      document.querySelector('.cart-modal').style.display='flex';
+  });
+
+document.getElementById('right').addEventListener('click', function(){
+document.querySelector('.cart-modal').style.display = 'none';
+});
+
